5fdac971725ea093aacf5ded5eba655f36fbdb99
Jared Imbler <jared@degreed.com>
2019-04-09 16:33:12 -0600

    pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Cache Valley Nursery`,
+    description: `Cache Valley Nursery is Northern Utah’s newest source for ornamental trees, shrubs, and perennials. We offer more than 200 varieties, specializing in unusual, hard-to-find, and native selections.`,
+    author: `@jimbler`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
